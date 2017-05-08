4b3df8417096878b5ddd5cc00212371a840b4188
John Hannah <john@friendlymachine.net>
2017-05-08 15:29:22 -0400

    add tests, update package.json

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,8 +1,28 @@
 import metaScraper from '../src/index';
 
 const fbUrl = 'https://facebook.com';
-const twitterUrl = 'https://twitter.com';
+const twitterUrl = 'http://npr.org';
 
-test('It exists', () => {
-  expect(metaScraper.meta(fbUrl)).toBeTruthy();
+describe('Meta scraper', () => {
+  test('meta method returns an object', () => {
+    metaScraper.meta(fbUrl)
+      .then(data => {
+        expect(typeof data).toBe('object');
+        expect(Array.isArray(data.meta)).toBe(true);
+      })
+  });
+
+  test('og method returns an object', () => {
+    metaScraper.og(fbUrl)
+      .then(data => {
+        expect(typeof data).toBe('object');
+      })
+  });
+
+  test('twitter method returns an object', () => {
+    metaScraper.twitter(twitterUrl)
+      .then(data => {
+        expect(typeof data).toBe('object');
+      })
+  });
 })
