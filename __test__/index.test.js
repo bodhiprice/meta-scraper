0c91b59e8f23dbcdbad2dea0f63f3aa5ea67f2f3
John Hannah <john@friendlymachine.net>
2017-05-09 10:10:43 -0400

    update test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,28 +1,18 @@
+/* eslint-disable no-undef */
 import metaScraper from '../src/index';
 
 const fbUrl = 'https://facebook.com';
-const twitterUrl = 'http://npr.org';
 
 describe('Meta scraper', () => {
-  test('meta method returns an object', () => {
-    metaScraper.meta(fbUrl)
+  test('returns an object', () => {
+    metaScraper(fbUrl)
       .then(data => {
         expect(typeof data).toBe('object');
         expect(Array.isArray(data.meta)).toBe(true);
       })
+      .catch(error => {
+        expect(error).toBeTruthy();
+      });
   });
-
-  test('og method returns an object', () => {
-    metaScraper.og(fbUrl)
-      .then(data => {
-        expect(typeof data).toBe('object');
-      })
-  });
-
-  test('twitter method returns an object', () => {
-    metaScraper.twitter(twitterUrl)
-      .then(data => {
-        expect(typeof data).toBe('object');
-      })
-  });
-})
+});
+/* eslint-enable no-undef */
